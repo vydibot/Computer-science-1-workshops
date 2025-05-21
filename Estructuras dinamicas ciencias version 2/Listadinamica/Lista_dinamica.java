e5f4f613a7170f989334554c59511ac92ad555d4
Carlos Alberto Barriga Gamez <163328750+CarlosBarG@users.noreply.github.com>
2025-05-20 23:05:57 -0500

    V2 Trabajo ciencias

---
+++ Estructuras dinamicas ciencias version 2/Listadinamica/Lista_dinamica.java
@@ -0,0 +1,41 @@
+package Listadinamica;
+import Creacion.Enlace_nodos;
+import javax.swing.JOptionPane;
+
+public class Lista_dinamica {
+    public static void main(String[] args) {
+        int op1;
+        Enlace_nodos obj = new Enlace_nodos();
+
+        do {
+            op1 = Integer.parseInt(JOptionPane.showInputDialog("****** DIGITE OPCIÓN ****\n"   
+                + "1. Capturar (crear lista aleatoria)\n" 
+                + "2. Imprimir lista\n"
+                + "3. Retirar nodo\n"
+                + "4. Agregar varios nodos al final\n"
+                + "5. Salir"));
+
+            switch (op1) {
+                case 1:
+                    obj.captura();
+                    JOptionPane.showMessageDialog(null,"**** LISTA CREADA *****");
+                    break;
+                case 2:
+                    obj.impre();
+                    break;
+                case 3:
+                    obj.retiro();
+                    JOptionPane.showMessageDialog(null,"**** NODO RETIRADO *****");
+                    obj.impre();
+                    break;
+                case 4:
+                    obj.adicionarNodosFinal();
+                    JOptionPane.showMessageDialog(null,"**** NODOS AGREGADOS AL FINAL *****");
+                    obj.impre();
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        } while (op1 < 6);
+    }
+}
